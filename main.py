5b5e41ccda94d6c949c22d0c2502b9e6b3da194b
zeppombal <pombal.josemaria@gmail.com>
2021-10-25 16:27:32 +0100

    config yaml is still given in code, but can also be given throug terminal. just uncomment lines 5 and 6, and comment 7, 8

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from schelling import *
 import yaml
+import sys
 
-with open(Path('.').absolute() / 'configs' / 'base.yaml') as f:
+#with open(Path('.').absolute() / 'configs' / f'{sys.argv[1]}.yaml') as f:
+#    config = yaml.safe_load(f)
+with open(Path('.').absolute() / 'configs' / f'3groups_base.yaml') as f:
     config = yaml.safe_load(f)
 
 sim = Simulation(**config)
