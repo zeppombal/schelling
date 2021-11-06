ac0572628df6af2df83a25ce76ae584303c137d6
antoniolilaia <antoniolilaia2000@gmail.com>
2021-11-06 18:41:19 +0000

    configs

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ path = 'basemin03'
 path = 'basemin04'
 #run_experiment(path, runs=50)
 
+path = 'adaptivity02'
+run_experiment(path, runs=50)
+
+path = 'adaptivity03'
+run_experiment(path, runs=50)
+
+path = 'adaptivity04'
+run_experiment(path, runs=50)
+
 path = 'base_resource_smart'
 #run_experiment(path, runs=500)
 
