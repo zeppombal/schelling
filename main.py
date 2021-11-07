9e1d76abb9208b60f16802223aa5d3e57364ba51
d-leitao <diogoeiraleitao@gmail.com>
2021-11-07 20:34:46 +0000

    adap_resource_smart02

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ path = 'adapsmart04'
 #run_experiment(path, runs=50)
 
 path = 'adap_resource_smart02'
-run_experiment(path, runs=3)
+run_experiment(path, runs=50)
 
 path = 'adap_resource_smart03'
 run_experiment(path, runs=50)
