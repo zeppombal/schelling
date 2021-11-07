fc8c46dd2b27ac76b2f4e018cda31927944f9f0d
antoniolilaia <antoniolilaia2000@gmail.com>
2021-11-07 21:47:04 +0000

    smart

--- main.py
+++ main.py
@@ -37,16 +37,22 @@ path = 'adapsmart04'
 #run_experiment(path, runs=50)
 
 path = 'adap_resource_smart02'
-run_experiment(path, runs=50)
+#run_experiment(path, runs=50)
 
 path = 'adap_resource_smart03'
-run_experiment(path, runs=50)
+#run_experiment(path, runs=50)
 
 path = 'adap_resource_smart04'
-run_experiment(path, runs=50)
+#run_experiment(path, runs=50)
 
 path = '3groups_base'
-run_experiment(path, runs=50)
+#run_experiment(path, runs=50)
 
 path = '3groupsbasemin02'
+#run_experiment(path, runs=50)
+
+path = 'basesmart02'
+run_experiment(path, runs=50)
+
+path = 'basesmart03'
 run_experiment(path, runs=50)
